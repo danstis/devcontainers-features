98c4472a9bbf9d3df4ac7a41d9befb5f217d7c3f
Dan Anstis <dan@bsod.co.nz>
2022-12-10 09:28:05 +1000

    fix(gitversion): add libicu

--- src/gitversion/install.sh
+++ src/gitversion/install.sh
@@ -70,7 +70,7 @@ find_version_from_git_tags() {
 echo "Installing gitversion ..."
 
 # Install dependencies if missing
-check_packages curl git ca-certificates
+check_packages curl git libicu ca-certificates
 
 ARCHITECTURE="$(uname -m)"
 case $ARCHITECTURE in
