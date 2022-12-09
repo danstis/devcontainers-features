fb6f02f88a4099a4e0758afb28592505c391d300
Dan Anstis <dan@bsod.co.nz>
2022-12-10 08:52:22 +1000

    fix: update install script

--- src/gitversion/install.sh
+++ src/gitversion/install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+set -ex
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
 echo "Activating feature 'gitversion'"
 
-GITVERSION_VERSION=${VERSION}
+GITVERSION_VERSION=${VERSION:-"latest"}
 echo "The provided version is: $GITVERSION_VERSION"
 
 
@@ -11,56 +16,25 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-USER=${2:-"automatic"}
-
 export DEBIAN_FRONTEND=noninteractive
 
-# shellcheck source=/dev/null
-source /etc/os-release
-
-# Clean up
-cleanup() {
-case "${ID}" in
-    debian|ubuntu)
-      rm -rf /var/lib/apt/lists/*
-    ;;
-  esac
-}
-
-apt_get_update() {
-  case "${ID}" in
-    debian|ubuntu)
-      if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-        echo "Running apt-get update..."
-        apt-get update -y
-      fi
-    ;;
-  esac
-}
-
-check_git() {
-    if [ ! -x "$(command -v git)" ]; then
-        check_packages git
-    fi
+apt_get_update()
+{
+    echo "Running apt-get update..."
+    apt-get update -y
 }
 
 # Checks if packages are installed and installs them if not
 check_packages() {
-  case "${ID}" in
-    debian|ubuntu)
-      if ! dpkg -s "$@" >/dev/null 2>&1; then
-        apt_get_update
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            apt_get_update
+        fi
         apt-get -y install --no-install-recommends "$@"
-      fi
-    ;;
-    alpine)
-      if ! apk -e info "$@" >/dev/null 2>&1; then
-        apk add --no-cache "$@"
-      fi
-    ;;
-  esac
+    fi
 }
 
+# Figure out correct version of a three part version number is not passed
 find_version_from_git_tags() {
     local variable_name=$1
     local requested_version=${!variable_name}
@@ -73,39 +47,43 @@ find_version_from_git_tags() {
         local escaped_separator=${separator//./\\.}
         local last_part
         if [ "${last_part_optional}" = "true" ]; then
-            last_part="(${escaped_separator}[0-9]+)*?"
+            last_part="(${escaped_separator}[0-9]+)?"
         else
             last_part="${escaped_separator}[0-9]+"
         fi
         local regex="${prefix}\\K[0-9]+${escaped_separator}[0-9]+${last_part}$"
-        local version_list
-        check_git
-        check_packages ca-certificates
-        version_list="$(git ls-remote --tags "${repository}" | grep -oP "${regex}" | tr -d ' ' | tr "${separator}" "." | sort -rV)"
+        local version_list="$(git ls-remote --tags ${repository} | grep -oP "${regex}" | tr -d ' ' | tr "${separator}" "." | sort -rV)"
         if [ "${requested_version}" = "latest" ] || [ "${requested_version}" = "current" ] || [ "${requested_version}" = "lts" ]; then
-            declare -g "${variable_name}"="$(echo "${version_list}" | head -n 1)"
+            declare -g ${variable_name}="$(echo "${version_list}" | head -n 1)"
         else
             set +e
-            declare -g "${variable_name}"="$(echo "${version_list}" | grep -E -m 1 "^${requested_version//./\\.}([\\.\\s]|$)")"
+            declare -g ${variable_name}="$(echo "${version_list}" | grep -E -m 1 "^${requested_version//./\\.}([\\.\\s]|$)")"
             set -e
         fi
     fi
-    if [ -z "${!variable_name}" ] || ! echo "${version_list}" | grep "^${!variable_name//./\\.}$" >/dev/null 2>&1; then
+    if [ -z "${!variable_name}" ] || ! echo "${version_list}" | grep "^${!variable_name//./\\.}$" > /dev/null 2>&1; then
         echo -e "Invalid ${variable_name} value: ${requested_version}\nValid values:\n${version_list}" >&2
         exit 1
     fi
     echo "${variable_name}=${!variable_name}"
 }
 
-echo "Installing mage ..."
+echo "Installing gitversion ..."
 
 # Install dependencies if missing
 check_packages curl ca-certificates
 
-# Soft version matching
-find_version_from_git_tags GITVERSION_VERSION "https://github.com/GitTools/GitVersion" "tags/v" "." "true"
+architecture="$(uname -m)"
+case $architecture in
+    x86_64) architecture="amd64";;
+    aarch64 | armv8* | arm64) architecture="arm64";;
+    *) echo "(!) Architecture $architecture unsupported"; exit 1 ;;
+esac
 
-curl -sL -o /tmp/gitversion.tar.gz "https://github.com/GitTools/GitVersion/releases/download/${GITVERSION_VERSION}/gitversion-linux-x64-${GITVERSION_VERSION}.tar.gz"
+# Soft version matching
+find_version_from_git_tags GITVERSION_VERSION "https://github.com/GitTools/GitVersion" "tags/"
+
+curl -sSL --fail -o /tmp/gitversion.tar.gz "https://github.com/GitTools/GitVersion/releases/download/${GITVERSION_VERSION}/gitversion-linux-x64-${GITVERSION_VERSION}.tar.gz"
 tar xvf /tmp/gitversion.tar.gz "gitversion"
 
 mv "gitversion" /usr/local/bin
@@ -113,6 +91,7 @@ rm -rf  /tmp/gitversion.tar.gz
 
 gitversion version
 
-cleanup
+# Clean up
+rm -rf /var/lib/apt/lists/*
 
 echo "Done!"
