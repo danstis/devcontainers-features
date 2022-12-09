6e3aeebd9f2077d6b60fa9cbc841c39388f657c6
Dan Anstis <dan@bsod.co.nz>
2022-12-10 09:30:04 +1000

    fix: change to libcurl

--- src/gitversion/install.sh
+++ src/gitversion/install.sh
@@ -70,7 +70,7 @@ find_version_from_git_tags() {
 echo "Installing gitversion ..."
 
 # Install dependencies if missing
-check_packages curl git libicu ca-certificates
+check_packages curl git libcurl ca-certificates
 
 ARCHITECTURE="$(uname -m)"
 case $ARCHITECTURE in
